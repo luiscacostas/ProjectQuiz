625c075e276204f96932eba7bc74836bafd227d0
luiscacostas <acosta.luisc15@gmail.com>
2024-06-21 00:04:53 +0200

    readme subido

--- README.md
+++ README.md
@@ -1,1 +1,279 @@
-# ProjectQuiz
+# ProjectQuiz# Proyecto Quiz
+Este proyecto es una aplicación de quiz que permite a los usuarios registrarse, iniciar sesión y participar en un juego de preguntas y respuestas. La aplicación también almacena y muestra las puntuaciones de los usuarios.
+
+------------
+
+
+####  Tabla de Contenidos
+1. Instalación
+2. Uso
+3. Código
+4. Configuración de Firebase
+5. Autenticación de Usuarios
+6. Juego de Quiz
+7. Mostrar Resultados
+8. Contribuciones
+9. Licencia
+10. Autores
+11. Instalación
+
+##### Para instalar y configurar el proyecto, sigue estos pasos:
+
+##### Clona el repositorio a tu máquina local.
+git clone https://github.com/tu-usuario/proyecto-grupal-quiz.git
+
+------------
+
+Los usuarios pueden registrarse y autenticarse utilizando Firebase Authentication.
+Se soporta el inicio de sesión mediante Google.
+
+#### Juego de Quiz:
+
+Los usuarios pueden jugar un quiz de 10 preguntas obtenidas de Open Trivia Database.
+Se muestra el temporizador y se almacenan las respuestas.
+Puntuaciones y Resultados:
+
+Al final del quiz, se muestra la puntuación obtenida.
+Las puntuaciones se almacenan localmente y se muestran en un gráfico.
+
+Configuración de Firebase.
+El proyecto utiliza Firebase para la autenticación de usuarios y Firestore para almacenar los datos de los jugadores.
+
+```javascript
+const firebaseConfig = {
+    apiKey: "your-api-key",
+    authDomain: "proyecto-grupal-quiz.firebaseapp.com",
+    projectId: "proyecto-grupal-quiz",
+    storageBucket: "proyecto-grupal-quiz.appspot.com",
+    messagingSenderId: "596977327919",
+    appId: "1:596977327919:web:21c896093f84fd41274bda"
+};
+```
+
+```javascript
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+```
+
+------------
+
+
+##### Autenticación de Usuarios
+El código siguiente maneja la persistencia de sesión y la autenticación con Google.
+
+```javascript
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    .then(() => {
+        console.log("Persistencia de sesión configurada a LOCAL");
+    })
+    .catch((error) => {
+        console.error("Error configurando la persistencia de sesión:", error.message);
+    });
+```
+
+```javascript
+let provider = new firebase.auth.GoogleAuthProvider();
+auth.languageCode = "es";
+
+const loginGoogle = async () => {
+    try {
+        const response = await firebase.auth().signInWithPopup(provider);
+        console.log(response);
+        return response.user;
+    } catch (error) {
+        throw new Error(error)
+    }
+};
+```
+##### Juego de Quiz
+El siguiente código maneja la obtención de datos del API, la visualización del quiz y el temporizador.
+
+```javascript
+const getData = async () => {
+    try {
+        const resp = await fetch('https://opentdb.com/api.php?amount=10');
+        if (resp.ok) {
+            const data = await resp.json();
+            results = data.results;
+            console.log(results);
+            printQuiz(results, index);
+        } else {
+            throw new Error('Error al obtener los datos');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+```
+```javascript
+const printQuiz = (results, i) => {
+    startTimer();
+    if (index === 10) {
+        printResults(respuestas);
+        return;
+    }
+
+    quiz.innerHTML = '';
+    options = shuffle([...results[i].incorrect_answers, results[i].correct_answer]);
+    const pregunta = document.createElement('H3');
+    pregunta.classList.add('styleParrafoPregunta');
+    pregunta.textContent = results[i].question;
+    quiz.append(pregunta);
+
+    options.forEach((opc, index) => {
+        const buttonOption = document.createElement('BUTTON');
+        buttonOption.textContent = opc;
+        buttonOption.setAttribute('id', `option${index + 1}`);
+        buttonOption.classList.add('btn_option_style');
+        buttonOption.classList.add(`option${index + 1}`);
+        buttonOption.value = opc;
+        quiz.append(buttonOption);
+    });
+
+    const buttonNext = document.createElement('INPUT');
+    buttonNext.setAttribute('type', 'submit');
+    buttonNext.value = 'Siguiente';
+    buttonNext.setAttribute('id', 'siguiente');
+    buttonNext.disabled = true;
+    quiz.append(buttonNext);
+
+    const buttonHome = document.createElement('BUTTON');
+    buttonHome.textContent = 'Volver al Menu Principal';
+    buttonHome.classList.add('buttonHomeStyle');
+    quiz.append(buttonHome);
+
+    const divReloj = document.createElement('DIV');
+    const timeSpan = document.createElement('P');
+    divReloj.setAttribute('id', 'timer');
+    timeSpan.setAttribute('id', 'time');
+    timeSpan.textContent = 10;
+    divReloj.append(timeSpan);
+    quiz.append(divReloj);
+
+    buttonHome.addEventListener('click', () => {
+        window.location.href = 'home.html';
+    });
+};
+
+function startTimer() {
+    timeLeft = 10;
+    clearInterval(timer);
+    timer = setInterval(() => {
+        const timerElement = document.getElementById('time');
+        if (!timerElement) return;
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            validateResponse(null, null, true);
+            disableButtons();
+            document.getElementById('time').textContent = 'Tiempo Agotado';
+        } else {
+            timeLeft--;
+            document.getElementById('time').textContent = timeLeft;
+        }
+    }, 1000);
+}
+```
+##### Mostrar Resultados
+
+El siguiente código maneja la visualización de los resultados y la creación de un gráfico con las puntuaciones.
+
+```javascript
+const printResults = (respuestas) => {
+    quiz.innerHTML = '';
+    const puntuacion = respuestas.reduce((acc, sum) => acc += sum, 0);
+    const divPuntuacion = document.createElement('DIV');
+    divPuntuacion.classList.add('divPuntuacion');
+    const pResultado = document.createElement('P');
+    const textoResultado = document.createElement('P');
+    textoResultado.textContent = 'Tu resultado';
+    textoResultado.classList.add('textoChart');
+    pResultado.textContent = `${puntuacion} / 10`;
+    pResultado.classList.add('stylePuntuacion');
+    divPuntuacion.append(pResultado);
+
+    const newScore = {
+        date: fecha,
+        points: puntuacion
+    };
+    scores.push(newScore);
+    localStorage.setItem("scores", JSON.stringify(scores));
+    const storedScores = JSON.parse(localStorage.getItem("scores"));
+    const divChartContainer = document.createElement('DIV');
+    divChartContainer.classList.add('divChartContainer', 'ct-chart', 'ct-perfect-fourth', 'styleGrafica');
+    const textoChart = document.createElement('P');
+    textoChart.classList.add('textoChart');
+    textoChart.textContent = 'Tus puntuaciones';
+    const btnReturnPlay = document.createElement('BUTTON');
+    btnReturnPlay.textContent = 'Volver a Jugar';
+    btnReturnPlay.classList.add('btnReturnPlay');
+    const divScores = document.createElement('DIV');
+    divScores.classList.add('styleGrafica');
+    storedScores.forEach(score => {
+        const logScores = document.createElement('P');
+        logScores.innerHTML = `Fecha: ${score.date} - <strong>Puntuación: ${score.points}</strong>`;
+        divScores.append(logScores);
+    });
+
+    quiz.append(textoResultado, divPuntuacion, textoChart, divChartContainer, btnReturnPlay);
+
+    btnReturnPlay.addEventListener('click', () => {
+        index = 0;
+        respuestas = [];
+        score.points = 0;
+        getData();
+    });
+
+    const data = {
+        labels: scores.map(resultado => resultado.date),
+        series: [scores.map(resultado => resultado.points)]
+    };
+    const options = {
+        showPoint: false,
+        showArea: true,
+        fullWidth: false,
+        chartPadding: {
+            top: 40,
+            right: 20,
+            bottom: 40
+        },
+        axisX: {
+            showGrid: true
+        },
+        axisY: {
+            low: 0,
+            high: 10,
+            onlyInteger: true,
+            referenceValue: 5,
+            showGrid: false
+        }
+    };
+    const chart = new Chartist.Line('.ct-chart', data, options);
+    chart.on('draw', function (context) {
+        if (context.type === 'point') {
+            context.element.attr({
+                style: 'stroke: rgb(255, 87, 199); stroke-width: 12px;'
+            });
+        }
+        if (context.type === 'line') {
+            context.element.attr({
+                style: 'stroke: rgb(255, 87, 199);'
+            });
+        }
+    });
+};
+```
+##### Persistencia de Datos
+Los datos de las puntuaciones se almacenan en localStorage para ser recuperados y mostrados posteriormente.
+
+##### Manejo del Temporizador
+El temporizador controla el tiempo de respuesta para cada pregunta. Si el tiempo se agota, se pasa a la siguiente pregunta automáticamente.
+
+##### Manejo de Errores
+El manejo de errores se realiza principalmente mediante bloques try-catch para capturar y manejar errores en la obtención de datos del API y la autenticación de usuarios.
+
+Autores
+Luis Carlos Acosta - Full Stack.
+Eduardo Fatou - Full Stack.
+Roberto Ruano - Full Stack.
--- script.js
+++ script.js
@@ -5,10 +5,18 @@ const firebaseConfig = {
     storageBucket: "proyecto-grupal-quiz.appspot.com",
     messagingSenderId: "596977327919",
     appId: "1:596977327919:web:21c896093f84fd41274bda"
-  };
+};
 
 firebase.initializeApp(firebaseConfig);// Inicializaar app Firebase
 const auth = firebase.auth();
+
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    .then(() => {
+        console.log("Persistencia de sesión configurada a LOCAL");
+    })
+    .catch((error) => {
+        console.error("Error configurando la persistencia de sesión:", error.message);
+    });
 
 let provider = new firebase.auth.GoogleAuthProvider();
 
@@ -42,21 +50,39 @@ const container1 = document.querySelector('#modal-container1');
 const container2 = document.querySelector('#modal-container2');
 let imagenPerfil = document.querySelector('.profileImagen')
 let divImagenFav = document.querySelector('.subirImagen')
+let dialogImagen = document.querySelector('#subirImagen')
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            console.log("Usuario está logueado:", user.email);
+            menuPlayer();
+            displayImage(user.photoURL || '../assets/image_defecto.png');
+        } else {
+            console.log("Usuario no está logueado");
+        }})
+
     if (btnOptions) {
-        btnOptions.addEventListener('click', (ev)=> {
-        if(ev.target.value == 'salir del perfil'){
-            console.log("dentro del boton salir")
-            signOutPlayer()
-        } else if (ev.target.value == 'cambiar imagen'){
-            const subirImagen = document.querySelector('#subirImagen')
-            subirImagen.showModal();
-        } 
+        btnOptions.addEventListener('click', (ev) => {
+            if (ev.target.value == 'salir del perfil') {
+                console.log("dentro del boton salir")
+                signOutPlayer()
+            } else if (ev.target.value == 'cambiar imagen') {
+                const subirImagen = document.querySelector('#subirImagen')
+                const archivoImagen = document.querySelector('#archivoImagen')
+                subirImagen.showModal();
+                if (archivoImagen.style.display == 'none'){
+                    archivoImagen.style.display = '';
+                }
+            } else if (ev.target.value == 'borrar cuenta'){
+                deletePlayer();
+                
+            }
+        }
+
+        )
     }
-        
-)}
 
 
     if (btnHome) {
@@ -96,38 +122,38 @@ document.addEventListener('DOMContentLoaded', () => {
     if (container2) {
         container2.addEventListener('click', async (ev) => {
             if (ev.target.id === 'btnGoogle') {
-                try{
-                await loginGoogle();
-                menuPlayer();
-                const user = firebase.auth().currentUser
-                console.log(user.uid, user.email)
-                console.log("he cerrado la ventana")
-                console.log(user.uid, user.email)
-                createPlayer({
-                    id: user.uid,
-                    email: user.email,
-                    imagen: "gs://proyecto-grupal-quiz.appspot.com/images/yGWbojjZ0ucnertBphwkNtCShcg2.jpg",
-                });
-                container2.close();
-                console.log("he creado el usuario en la BBDD")
-             } catch (error){}
+                try {
+                    await loginGoogle();
+                    menuPlayer();
+                    const user = firebase.auth().currentUser
+                    console.log(user.uid, user.email)
+                    console.log("he cerrado la ventana")
+                    console.log(user.uid, user.email)
+                    createPlayer({
+                        id: user.uid,
+                        email: user.email,
+                        imagen: "gs://proyecto-grupal-quiz.appspot.com/images/yGWbojjZ0ucnertBphwkNtCShcg2.jpg",
+                    });
+                    container2.close();
+                    console.log("he creado el usuario en la BBDD")
+                } catch (error) { }
             }
         })
     }
 });
 
-document.addEventListener('submit', (event)=>{
+document.addEventListener('submit', (event) => {
     event.preventDefault();
-    if (event.target.id == "form1"){
+    if (event.target.id == "form1") {
         let email = event.target.elements.email.value;
         let password = event.target.elements.pass.value;
         signUpPlayer(email, password)
-    } else if (event.target.id == "form2"){
+    } else if (event.target.id == "form2") {
         let email = event.target.elements[0].value;
         let password = event.target.elements[1].value;
-        
-        loginPlayer(email,password)
-    } 
+
+        loginPlayer(email, password)
+    }
 })
 
 const getData = async () => {
@@ -152,7 +178,7 @@ const printQuiz = (results, i) => {
         printResults(respuestas);
         return;
     }
-    
+
     quiz.innerHTML = '';
     options = shuffle([...results[i].incorrect_answers, results[i].correct_answer]);
     const pregunta = document.createElement('H3');
@@ -190,7 +216,7 @@ const printQuiz = (results, i) => {
     divReloj.append(timeSpan)
     quiz.append(divReloj)
 
-    buttonHome.addEventListener('click', ()=>{
+    buttonHome.addEventListener('click', () => {
         window.location.href = 'home.html';
     })
 
@@ -224,11 +250,11 @@ const validateResponse = (value, button, timeOut = false) => {
         if (value === results[index].correct_answer) {
             button.classList.add('styleOptionActive');
             respuestas.push(1);
-        clearInterval(timer);
+            clearInterval(timer);
         } else {
             respuestas.push(0);
             button.classList.add('styleOptionInactive');
-        clearInterval(timer);
+            clearInterval(timer);
         }
     } else {
         respuestas.push(0);
@@ -239,7 +265,7 @@ const validateResponse = (value, button, timeOut = false) => {
     }
     disableButtons()
     document.getElementById('siguiente').disabled = false;
-    
+
 };
 
 const disableButtons = () => {
@@ -275,17 +301,17 @@ const printResults = (respuestas) => {
     textoChart.classList.add('textoChart')
     textoChart.textContent = 'Tus puntuaciones'
     const btnReturnPlay = document.createElement('BUTTON')
-    btnReturnPlay.textContent='Volver a Jugar'
+    btnReturnPlay.textContent = 'Volver a Jugar'
     btnReturnPlay.classList.add('btnReturnPlay')
     const divScores = document.createElement('DIV');
     divScores.classList.add('styleGrafica');
     storedScores.forEach(score => {
         const logScores = document.createElement('P');
-        logScores.innerHTML = `Fecha: ${score.date} - <strong>PuntuaciÃ³n: ${score.points}</strong>`;
+        logScores.innerHTML = `Fecha: ${score.date} - <strong>Puntuación: ${score.points}</strong>`;
         divScores.append(logScores);
     });
 
-    quiz.append(textoResultado, divPuntuacion, textoChart, divChartContainer,btnReturnPlay)
+    quiz.append(textoResultado, divPuntuacion, textoChart, divChartContainer, btnReturnPlay)
 
     btnReturnPlay.addEventListener('click', () => {
         index = 0;
@@ -335,7 +361,7 @@ const printResults = (respuestas) => {
                 style: 'fill: rgb(255, 0, 170);'
             });
         }
-    }); 
+    });
     chart.on('created', function () {
         const axisXLabels = document.querySelectorAll('.ct-label.ct-horizontal');
         axisXLabels.forEach(function (label) {
@@ -349,7 +375,7 @@ const printResults = (respuestas) => {
             linePath.style.transition = 'none';
             linePath.style.strokeDasharray = length + ' ' + length;
             linePath.style.strokeDashoffset = length;
-            linePath.getBoundingClientRect(); // Forzar el reflujo para reiniciar la animaciÃ³n
+            linePath.getBoundingClientRect(); // Forzar el reflujo para reiniciar la animación
             linePath.style.transition = 'stroke-dashoffset 2s ease-out';
             linePath.style.strokeDashoffset = '0';
         }
@@ -371,36 +397,42 @@ const validateInicio = (valueOption) => {
     } else if (valueOption == 'login') {
         console.log('login')
         container2.showModal();
-    } else if(valueOption == 'Opciones'){
+    } else if (valueOption == 'Opciones') {
         popUpOpciones.showModal();
-    } else if(valueOption == 'salir del perfil'){
+    } else if (valueOption == 'salir del perfil') {
         console.log("dentro del boton salir")
         signOutPlayer()
     }
 
-     cancelarbtn.forEach((btn)=>{
-        btn.addEventListener('click',()=>{
+    cancelarbtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
             container1.close()
             container2.close()
             popUpOpciones.close()
             container3.close()
             subirImagen.close()
-    })  
-})
+        })
+    })
 };
 
 // AUTENTICACION
 
-const loginGoogle = async () =>{
- try {const response = await firebase.auth().signInWithPopup(provider);
- console.log(response);
- return response.user;
- } catch (error){
-    throw new Error(error)
- }
+const loginGoogle = async () => {
+    const user = firebase.auth().currentUser
+    try {
+        const response = await firebase.auth().signInWithPopup(provider);
+        console.log(response);
+        if (user.photoURL) {
+            displayImage(user.photoURL);
+        } else {displayImage()}
+        return response.user;
+    } catch (error) {
+        throw new Error(error)
+    }
 };
 
 const loginPlayer = async (email, password) => {
+    const user = firebase.auth().currentUser
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
             const container2 = document.querySelector('#modal-container2');
@@ -408,10 +440,17 @@ const loginPlayer = async (email, password) => {
             let player = userCredential.user;
             console.log(`se ha logado ${player.email} ID:${player.uid}`)
             if (container2.open == true) {
-                container2.close()}//cierra el popUp si esta abierto
+                container2.close()
+            }//cierra el popUp si esta abierto
             alert(`se ha logado ${player.email} ID:${player.uid}`)
             console.log("PLAYER", player);
-            //cambia el menÃº y muetra la imagen de usuario
+            //cambia el menú y muetra la imagen de usuario
+            if (user.photoURL) {
+                console.log(user.photoURL)
+                displayImage(user.photoURL);
+            }else {
+                displayImage()
+            };
             menuPlayer();
         })
         .catch((error) => {
@@ -443,7 +482,7 @@ const signUpPlayer = (email, password) => {
         .then(async (userCredential) => {
             let user = userCredential.user;
             console.log(`se ha registrado ${email1} ID:${user.uid}`)
-            alert(`se ha registrado ${email1} con Ã©xito`)
+            alert(`se ha registrado ${email1} con éxito`)
             await loginPlayer(email1, password1)
             // ...
             // Saves user in firestore
@@ -468,7 +507,21 @@ const createPlayer = (player) => {
         .catch((error) => console.error("Error adding document: ", error));
 };
 
-const menuPlayer = ()=>{
+const deletePlayer = async () => {
+    const user = firebase.auth().currentUser;
+    const userEmail = user.email;
+    db.collection('player').where('email', '==', userEmail).get()
+    .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            doc.ref.delete(); // Elimina el documento encontrado
+        });
+    })
+    .catch(error => {
+        console.error('Error al buscar y eliminar documento:', error);
+    });
+}
+
+const menuPlayer = () => {
     const btnLogin = document.querySelector('#btnLogin');
     const btnRegister = document.querySelector('#btnRegistro');
     const btnContenedor = document.querySelector('#btnContenedor');
@@ -485,35 +538,38 @@ const menuPlayer = ()=>{
     btnContenedor.append(btnOpciones)
 };
 
-const uploadFile = ()=> {
+//imagen usuario
+
+const uploadFile = () => {
     const file = document.getElementById("files").files[0];
     const user = firebase.auth().currentUser;
     const storageRef = firebase.storage().ref();
     const thisRef = storageRef.child(`images/${user.uid}.jpg`);
 
-    thisRef.put(file).then((snapshot) =>{
+    thisRef.put(file).then((snapshot) => {
         alert("Imagen subida");
         console.log('Imagen subida correctamente!');
         return thisRef.getDownloadURL();
-    }).then((url)=> {
+    }).then((url) => {
         return user.updateProfile({
             photoURL: url
-        }).then(()=> {
+        }).then(() => {
             console.log("Perfil actualizado con la nueva imagen");
             displayImage(url);
+            dialogImagen.close();
         });
-    }).catch((error)=> {
+    }).catch((error) => {
         console.error("Error al subir imagen o actualizar perfil: ", error);
         alert("Error al subir imagen o actualizar perfil.");
     });
 }
 document.getElementById("uploadButton").addEventListener("click", uploadFile);
 
-const displayImage = (url)=> {
+const displayImage = (url = '../assets/image_defecto.png') => {
     const img = document.createElement('img');
     img.src = url;
     img.classList.add('styleProfile')
     imagenPerfil.innerHTML = '';
     imagenPerfil.append(img);
-    divImagenFav.style.display= 'none'
+    divImagenFav.style.display = 'none'
 }
